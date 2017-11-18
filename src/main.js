9f88e1ca85d6b7526fd5f2812d3b90e5a42231ba
hujj0908 <huwanqi@shuwen.com>
2017-11-19 04:00:30 +0800

    vue awesome swiper bug fix

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import 'swiper/dist/css/swiper.css'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import App from './App'
 import router from './router'
 
 import util from './util'
 
 Vue.config.productionTip = false
+
+Vue.use(VueAwesomeSwiper)
 
 Vue.directive('focus', {
   inserted: function(el) {
